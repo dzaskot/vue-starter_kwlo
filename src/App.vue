8de006e4ea9261404e64ae4d3e89e9ffb29f1e50
Kaska <wlodarczyk@student.agh.edu.pl>
2020-05-08 00:58:49 +0200

    New Component

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,39 @@
 <template>
   <div>
     <h1>Witaj w systemie zapisów na zajęcia</h1>
-    <div v-if= "authenticatedUserName != ''">
+    <div v-if= "authenticatedUserName !==''">
       <h3>Witaj {{authenticatedUserName}}</h3>
       <a @click="logOut()">Wyloguj </a>
     </div>
     <div v-else>
-      <label>Zaloguj się emailem </label>
+      <login-form @login="logIn($event)"></login-form>
+      <!--<label>Zaloguj się emailem </label>
       <input type="email" v-model = "email">
-      <button class="myButton" @click="logIn()"> Zaloguj</button>
+      <button class="myButton" @click="logIn()"> Zaloguj</button>-->
     </div>
   </div>
 </template>
 
 <script>
+import LoginForm from "./LoginForm";
 import "milligram";
 
 export default {
+  components: {LoginForm},
   data() {
     return {
-      email: '',
       authenticatedUserName:''
     };
   },
   methods: {
-    logIn(email) {
-      this.authenticatedUserName = this.email;
+    logIn(username) {
+      this.authenticatedUserName = username;
     },
-    logOut(email) {
-      this.email = '';
+    logOut() {
       this.authenticatedUserName = '';
     }
   }
 }
 </script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.myButton {
-  margin-left: 5px
-}
 </style>
