b884071ab4fc073deb82656ee02af9e0ace2e5a0
Kaska <wlodarczyk@student.agh.edu.pl>
2020-05-09 06:31:21 +0200

    Logout

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,21 @@
   <div>
     <h1>Witaj w systemie zapisów na zajęcia</h1>
     <div v-if= "authenticatedUserName !==''">
-      <h3>Witaj {{authenticatedUserName}}</h3>
-      <a @click="logOut()">Wyloguj </a>
+      <logout :username="authenticatedUserName" @logout="logOut()"></logout>
     </div>
     <div v-else>
-      <login-form @login="logIn($event)"></login-form>
-      <!--<label>Zaloguj się emailem </label>
-      <input type="email" v-model = "email">
-      <button class="myButton" @click="logIn()"> Zaloguj</button>-->
+      <login-form @login="logIn($event)" button-label="Zaloguj się"></login-form>
     </div>
   </div>
 </template>
 
 <script>
 import LoginForm from "./LoginForm";
+import Logout from "./Logout";
 import "milligram";
 
 export default {
-  components: {LoginForm},
+  components: {LoginForm,Logout},
   data() {
     return {
       authenticatedUserName:''
